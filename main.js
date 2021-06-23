d1e8a424e4a5359ce0f0bca17236c41264e18de1
Saemmool Lee <leesm0426@gmail.com>
2021-06-24 07:00:45 +0900

    [update] added javascript

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// const profileName =document.querySelectorsAll('.profname');
+// const profileButton = document.querySelectorAll('.btn');
+
+// function selectItem(e) {
+//     removeName();
+//     removeButton();
+//     //add button 
+//     this.classList.add('.profname');
+//     this.classlist.add('.btn');
+// }
+
+// function removeName() {
+//     profileName.forEach(item => item.classList.remove('.profname'));
+// }
+
+// function removeButton() {
+//     profileButton.forEach(item => item.classList.remove('.btn'));
+// }
+
+// profileName.forEach(item => item.addEventListener('click', selectItem));
